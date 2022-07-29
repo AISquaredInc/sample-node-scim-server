5504dd89013b2ca33faa6c74d9bcc62940ebfab1
Rafael E. ONeill <rafael.oneill@squared.ai>
2022-07-29 14:43:42 -0400

    Changed env variable format

--- core/Database.js
+++ core/Database.js
@@ -27,8 +27,8 @@ let config = {
     connection: {
         host: process.env["POSTGRESQL_HOST"],
         port: process.env["POSTGRESQL_PORT"],
-        user: process.env["POSTGRESQL-USER"],
-        password: process.env["POSTGRESQL-PASSWORD"],
+        user: process.env["POSTGRESQL_USER"],
+        password: process.env["POSTGRESQL_PASSWORD"],
         database: process.env["POSTGRESQL_DATABASE"]
     }
 }
